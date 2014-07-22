46e52e19aa3e99e0c795813bab96002896c2e046
Nima Jahanshahi <nbjahan@gmail.com>
2014-07-22 12:16:52 +0430

    Added support for update checking

--- src/pinboard/view-config.go
+++ src/pinboard/view-config.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"path"
 	"time"
 
+	"github.com/DHowett/go-plist"
 	. "github.com/nbjahan/go-launchbar"
 )
 
@@ -65,6 +68,48 @@ func init() {
 		c.Cache.Clean()
 
 		c.Action.ShowView("main")
+	})
+
+	i = v.NewItem("Check for Action Updates ...")
+	i.SetRender(func(c *Context) {
+		version := pb.Config.GetString("newversion")
+		if version != "" {
+			c.Self.SetSubtitle(fmt.Sprintf("latest version: %q, you have: %q", version, pb.Version()))
+		} else {
+			c.Self.SetSubtitle("click to check")
+		}
+	})
+	i.SetActionRunsInBackground(false)
+	i.SetIcon("/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/ToolbarDownloadsFolderIcon.icns")
+	i.SetRun(func(c *Context) Items {
+		v := pb.NewView("")
+		version := ""
+		if resp, err := http.Get("https://raw.githubusercontent.com/nbjahan/launchbar-pinboard/master/src/Info.plist"); err == nil {
+			defer resp.Body.Close()
+			if data, err := ioutil.ReadAll(resp.Body); err == nil {
+				var v map[string]interface{}
+				if _, err := plist.Unmarshal(data, &v); err == nil {
+					version = v["CFBundleVersion"].(string)
+				}
+			}
+		}
+		if version == "" {
+			v.NewItem("Cannot get the version").SetIcon("Alert").SetSubtitle("Please try again later.").SetAction("")
+		} else {
+			c.Config.Set("newversion", version)
+			if c.Action.Version().Cmp(Version(version)) < 0 {
+				dl := fmt.Sprintf("https://github.com/nbjahan/launchbar-pinboard/releases/download/v%s/Pinboard-%s.lbext",version,version)
+				v.NewItem(fmt.Sprintf("Download New version v%s (I'm v%s)", version, c.Action.Version())).
+					SetIcon("/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/ToolbarDownloadsFolderIcon.icns").
+					SetURL(dl).SetSubtitle(dl).SetAction("")
+			} else {
+				v.NewItem("I'm up to date!").SetAction("").
+					SetSubtitle(fmt.Sprintf("latest version is %s, I'm %s", version, c.Action.Version())).
+					SetIcon("at.obdev.LaunchBar:GreenCheckmark")
+			}
+		}
+		v.NewItem("Back").SetIcon("BackTemplate").Run("showView", "config")
+		return v.Items
 	})
 
 	i = v.NewItem("Paths")
